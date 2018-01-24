0940608d44db4fbf238c57a944e8b36d0d5b1bc4
fourside <fourside@gmail.com>
2018-01-25 06:51:21 +0900

    add type

--- src/renderer/lib/timer.ts
+++ src/renderer/lib/timer.ts
@@ -1,9 +1,10 @@
 'use strict';
 
-const CronJob = require('cron').CronJob;
+import { CronJob }  from 'cron';
+
 const timer = {
   jobs : [],
-  start: function(cronTime, cb) {
+  start: function(cronTime :string, cb :() => void) {
     const job = new CronJob({
       cronTime: cronTime,
       onTick: cb,
@@ -18,15 +19,21 @@ const timer = {
     });
     this.jobs = [];
   },
-  isValid: function(cronTime) {
+  isValid: function(cronTime :string) {
     try {
-      const j = new CronJob(cronTime);
+      const j = new CronJob(cronTime, () => {});
       j.stop();
       return true;
     } catch(ignore) {
       return false;
     }
   }
-}
+} as Timer;
 
-module.exports = timer;
+interface Timer {
+  jobs : CronJob[];
+  start(cronTime :string, cb :() => void) :void;
+  stop() :void
+  isValid(cronTime :string) :boolean
+}
+export default timer;
